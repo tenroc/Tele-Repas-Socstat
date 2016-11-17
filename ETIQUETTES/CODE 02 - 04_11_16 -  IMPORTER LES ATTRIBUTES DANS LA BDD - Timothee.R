d676696e0f970ba1eb67f3e6fcca2c6286f66d6d
Unknown <marie.plessz@inra.fr>
2016-11-17 16:38:21 +0100

    Suppression du code etiquettes obsolete

--- ETIQUETTES/CODE 02 - 04_11_16 -  IMPORTER LES ATTRIBUTES DANS LA BDD - Timothee.R
+++
@@ -1,7 +0,0 @@
-enq.final <- read.csv("enq final.csv")
-etiquettes <- read.csv("etiquettes.csv")
-etiquettes <- as.character(etiquettes$x)
-attributes(enq.final)$variable.labels <- etiquettes
-attributes(enq.final)$variable.labels[120]
-
-

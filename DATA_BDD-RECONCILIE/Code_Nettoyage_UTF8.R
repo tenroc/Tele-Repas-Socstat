9df6c8db95f81cf8fd07d63a0166e7a7231ec4b2
Sarah B <sarah.benmarhnia@gmail.com>
2016-12-01 12:32:25 +0100

    Exportation d'une nouvelle version de la Base de données
    enq.final_v2

--- DATA_BDD-RECONCILIE/Code_Nettoyage_UTF8.R
+++ DATA_BDD-RECONCILIE/Code_Nettoyage_UTF8.R
@@ -2821,3 +2821,7 @@ table(enq.final$D1_re, useNA = "ifany")
 table(enq.final$D13_re, useNA = "ifany")
 table(enq.final$A1_re, useNA = "ifany")
 table(enq.final$D5_re, useNA = "ifany")
+
+
+
+write.csv2(enq.final,"./_Data/BDD réconciliée/BDD/enq final_v2.csv", fileEncoding = "UTF-8")

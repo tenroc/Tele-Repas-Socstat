86db890be4a8be0160ae1f896383df6452193e5d
Maxime Cornet <maxime.765@gmail.com>
2017-01-20 16:36:17 +0100

    Ajout du code ACM / partie equippement
    Le code tourne et donne une ACM lisible. J'ai commencé le recodage des variables socio-démo en prévision de la preojection des variables supplémentaires (à terminer).

--- Travaux Groupes/Groupe_equippement.R
+++ Travaux Groupes/Groupe_equippement.R
@@ -345,6 +345,66 @@ round(prop.table(table(enq.final$E7_Typologie_utilisateurs))*100,3)
 table(enq.final$E8_Typologie_utilisateurs)
 round(prop.table(table(enq.final$E8_Typologie_utilisateurs))*100,3)
 
+## Recodages
+
+# Age en tranches
+
+enq.final$D1_tr[enq.final$D1_re < 25] <- "moins de 25 ans"
+enq.final$D1_tr[enq.final$D1_re %in% c(25:35)] <- "25-35 ans"
+enq.final$D1_tr[enq.final$D1_re %in% c(36:45)] <- "36-45 ans"
+enq.final$D1_tr[enq.final$D1_re %in% c(46:55)] <- "46-55 ans"
+enq.final$D1_tr[enq.final$D1_re %in% c(56:65)] <- "56-65 ans"
+enq.final$D1_tr[enq.final$D1_re > 65] <- "plus de 65 ans"
+table(enq.final$D1_tr)
+
+# Diplome simplifié
+
+table(enq.final$D3_re)
+enq.final$D3_re2[as.character(enq.final$D3_re) %in% c("Aucun diplôme", "BEPC, brevet", "CAP, BEP","Certificat d’études primaires")] <- "Inférieur au bac"
+enq.final$D3_re2[as.character(enq.final$D3_re) == "Baccalauréat, BP"] <- "Bac"
+enq.final$D3_re2[as.character(enq.final$D3_re) %in% c("Deug, DUT, BTS, diplômes des professions sociales ou de la santé","Licence, Bac+3","Bac+4 ou plus : Master, maîtrise, DEA, école d'ingénieur, doctorat etc.")] <- "Supérieur au bac"
+enq.final$D3_re2[as.character(enq.final$D3_re) == "Refus"] <- NA
+table(enq.final$D3_re2)
+
+# Diplome du conjoin simplifié:
+
+table(enq.final$D3_bis_re)
+enq.final$D3_re2[as.character(enq.final$D3_bis_re) %in% c("Aucun diplôme", "BEPC, brevet", "CAP, BEP","Certificat d’études primaires")] <- "Inférieur au bac"
+enq.final$D3_re2[as.character(enq.final$D3_bis_re) == "Baccalauréat, BP"] <- "Bac"
+enq.final$D3_re2[as.character(enq.final$D3_bis_re) %in% c("Deug, DUT, BTS, diplômes des professions sociales ou de la santé","Licence, Bac+3","Bac+4 ou plus : Master, maîtrise, DEA, école d'ingénieur, doctorat etc.")] <- "Supérieur au bac"
+enq.final$D3_re2[as.character(enq.final$D3_bis_re) == "Refus"] <- NA
+table(enq.final$D3_re2)
+
+# profession
+
+table(enq.final$D4_re)
+enq.final$D4_re2[as.character(enq.final$D4_re) %in% c("Etudiant avec emploi", "Etudiant sans emploi")] <- "Etudiant"
+enq.final$D4_re2[as.character(enq.final$D4_re) %in% c("Emploi à plein temps", "Emploi à temps partiel")] <- "Emploi"
+enq.final$D4_re2[as.character(enq.final$D4_re) == "A la retraite"] <- "retraite"
+enq.final$D4_re2[as.character(enq.final$D4_re) == "En recherche d'emploi"] <- "recherche emploi"
+enq.final$D4_re2[as.character(enq.final$D4_re) == "Au foyer"] <- "Au foyer"
+enq.final$D4_re2[as.character(enq.final$D4_re) == "Autre (voir D4_other_re)"] <- NA
+table(enq.final$D4_re2)
+
+# activité du conjoint - utilité?
+
+table(enq.final$D7_re)
+enq.final$D7_re2[as.character(enq.final$D7_re) %in% c("Etudiant avec emploi", "Etudiant sans emploi")] <- "étudiants"
+enq.final$D7_re2[as.character(enq.final$D7_re) %in% c("Emploi à plein temps", "Emploi à temps partiel")] <- "Emploi"
+enq.final$D7_re2[as.character(enq.final$D7_re) == "A la retraite"] <- "retraite"
+enq.final$D7_re2[as.character(enq.final$D7_re) == "En recherche d’emploi"] <- "recherche emploi"
+enq.final$D7_re2[as.character(enq.final$D7_re) == "Au foyer"] <- "Au foyer"
+enq.final$D7_re2[as.character(enq.final$D7_re) == "Autre (voir D4_other_re)"] <- NA
+table(enq.final$D7_re2)
+
+# Composition 
+
+table(enq.final$D6_re)
+
+
+# Revenu tranche aggrégées:
+
+enq.final$D6_re2[as.character(enq.final$D6_re) %in% c("Vos parents et éventuels frères et sœurs", "Etudiant sans emploi")] <- "étudiants"
 
 ## ACM
 
@@ -354,11 +414,11 @@ library(GDAtools)
 # Substet (sans E6: pas assez d'individus en utilisateurs réguliers, pas fondamental de toutes manières)
 
 acm <- subset(enq.final, select=c(E2_Typologie_utilisateurs, E3_Typologie_utilisateurs, E4_Typologie_utilisateurs, E5_Typologie_utilisateurs,
-                                  E7_Typologie_utilisateurs, E8_Typologie_utilisateurs))
+                                  E7_Typologie_utilisateurs, E8_Typologie_utilisateurs, D1, D2, D3, D4, D5, D6, D7, D8 , D9, D10, D11, D12, D13))
 
 acm <- as.data.frame(lapply(acm, factor))
 
-res.acm <- MCA(acm, ncp=3, graph=T)
+res.acm <- MCA(acm, quali.sup=, ncp=3, graph=T)
 
 # Axes à conserver:
 
@@ -375,7 +435,7 @@ res.acm$var$cos2
 # Contribution moyenne
 seuil <- 100/nrow(res.acm$var$contrib)
 
-modatot <- which(res.acm$var$contrib[, 1]>seuil ## which est une condition, on ne prends ici que les valeurs de res.acm$var$contrib, dans la dimension 1, qui sont supÃ¨rieurs au seuil.
+modatot <- which(res.acm$var$contrib[, 1]>seuil 
                  | res.acm$var$contrib[, 2]>seuil
                  | res.acm$var$contrib[, 3]>seuil)
 modatot
@@ -417,3 +477,49 @@ plot.MCA(res.acm, invisible=c("ind","quali.sup"),
 plot.MCA(res.acm, invisible=c("ind","quali.sup"), title="Nuage des modalites actives Plan 2-3", axes=c(2,3), 
          autoLab="yes", unselect=1,
          selectMod=c(moda23))
+
+# Mieux axe 1-2: cadre vide
+
+plot(res.acm$var$coord[modatot, 1:2]*1.2, type="n",
+     xlab=paste0("Axe 1 (", round(res.acm$eig[1,2], 1), "%)"),
+     ylab=paste0("Axe 2 (", round(res.acm$eig[2,2], 1), "%)"),
+     main="Premier plan factoriel",
+     cex.main=1, cex.axis=0.8, cex.lab=0.7, font.lab=3, 
+     asp=1)
+abline(h=0, v=0, col="grey", lty=3, lwd=1)
+
+# Points et etiquettes
+points(res.acm$var$coord[modatot, 1:2],
+       col="black",
+       pch=c(15, 15, 16, 16, 17, 18, 5, 5, 6, 6))
+
+etiquettes <- rownames(res.acm$var$coord)
+print(etiquettes)
+etiquettes2 <- c("Smartphone: Non ou peu", "Smartphone: régulier", "Mobile: Non ou peu", "Mobile: régulier",
+                 "Tablettes: régulier", "Télé: Non ou peu", "Ordi portable: Non ou peu", "Ordi portable: régulier",
+                 "Ordi fixe: Non ou peu", "Ordi fixe: régulier")
+col <- c(1,2,1,2,2,1,1,2,1,2)
+
+# etiquettes
+
+text(res.acm$var$coord[modatot,1:2], labels=etiquettes2, 
+     col=col, cex=1, pos=c(1, 1, 1, 1, 1, 1, 1, 2, 1, 1))
+
+# Mieux axe 2-3: cadre vide
+
+plot(res.acm$var$coord[modatot, 2:3]*1.2, type="n", 
+     xlab=paste0("Axe 2 (", round(res.acm$eig[2,2], 1), "%)"),
+     ylab=paste0("Axe 3 (", round(res.acm$eig[3,2], 1), "%)"),
+     main="Second plan factoriel",
+     cex.main=1, cex.axis=0.8, cex.lab=0.7, font.lab=3, 
+     asp=1)
+abline(h=0, v=0, col="grey", lty=3, lwd=1)
+
+# Points
+points(res.acm$var$coord[modatot, 2:3],
+       col="black",
+       pch=c(15, 15, 16, 16, 17, 18, 5, 5, 6, 6))
+
+
+text(res.acm$var$coord[modatot,2:3], labels=etiquettes2,
+     col=col, cex=1, pos=c(4,1,1,3,1,2,4,3,1,4))
